1129485dd4f8cc260089448bcc07d5eb69ad5583
bijay1088 <bijaybaniya1088@gmail.com>
2024-04-25 12:10:40 +0100

    Updated page visual

--- src/components/homepicture/HomePicture.jsx
+++ src/components/homepicture/HomePicture.jsx
@@ -12,7 +12,7 @@ const HomePicture = () => {
             <h1>Welcome to Gandaki Studio</h1>
             <p>Established for over 15 years, Gandaki Studio is a leading photography and videography service provider. 
                 Specializing in capturing moments for various occasions, including weddings, ceremonies, and more, we are dedicated to delivering high-quality imagery and memorable experiences.</p>
-            <button className='cusBtn'>Read More</button>
+            {/* <button className='cusBtn'>Read More</button> */}
             
         </div>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -27,10 +27,10 @@ const Navbar = () => {
     <nav className={`cusContainer ${sticky?'dark-nav':''}`}>
         <img src={logo} alt="Gandaki Logo"  className='logo'/>
         <ul className={menu?'':'hide-burger'}>
-            <li><Link to='home' smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to='package' smooth={true} offset={-250} duration={500}>Package</Link></li>
-            <li><Link to='works' smooth={true} offset={-200} duration={500}>About Us</Link></li>
-            <li><Link to='contact' smooth={true} offset={-260} duration={500}><button className='cusBtn'>Contact</button></Link> </li>
+            <li><Link to='home' smooth={false} offset={0} duration={200}>Home</Link></li>
+            <li><Link to='package' smooth={false} offset={-250} duration={200}>Package</Link></li>
+            <li><Link to='works' smooth={false} offset={-200} duration={200}>About Us</Link></li>
+            <li><Link to='contact' smooth={false} offset={-260} duration={200}><button className='cusBtn'>Contact</button></Link> </li>
         </ul>
         <FontAwesomeIcon icon={faBars} className='burger' onClick={toggleMenu} />
 
